014a269a2d29c3f07533218c8d88ea6a14612478
Masood Ahmed56 <115347725+masaoodahmedsarki@users.noreply.github.com>
2024-03-08 13:18:35 +0500

    data types summary

---
+++ demo9.js
@@ -0,0 +1,38 @@
+// primative 
+
+// 7 data types: string , number , boolean, null , undefined , symbol , bigInt
+
+const score = 100
+const scoreValue = 100.1
+
+const isloggedIn = false
+const outsideTem = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId =Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 1234873985873487585n
+
+
+
+
+//Refrance (NON premative)
+
+// Array , object , functions
+
+const heros = ["Feroz khan", "Haroon",  "SHARUKH"]
+
+let myObj = {
+    name: "masood",
+    age: 19,
+}
+
+const myfunction = function(){
+    console.log("Masood Ahmed");
+}
+
+console.log(typeof isloggedIn)
+console.log(isloggedIn)
+
